0408f5cd81feb14fa225bbcff2674b4ffecd11a2
Shreya V <62530781+shreyavkd@users.noreply.github.com>
2025-02-19 07:54:21 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include "system.h"
-// HINT: COMPLETE THE INCLUDE STATEMENT
+// COMPLETED THE INCLUDE STATEMENTS
 
 
 /**
@@ -15,17 +15,12 @@ int main() {
     double maxDisplacement  = 0.6;
     double dt = 0.5;
     double L = 20.0;
-
-    // initialise the system
+    
     System system(N, maxDisplacement,dt,L,1234);
-
-    // total number of iterations
     int iterations = N*500;
-
+    
     for (int i = 0; i < iterations; ++i) {
-        // move one disk
         system.step();
-        // save a configuration
         system.save("confs/conf"+std::to_string(i));
     }
     return 0;
