3f0b66aab8d4ca77574174cb4f504f82b6438c1d
Shreya V <62530781+shreyavkd@users.noreply.github.com>
2025-02-19 08:02:11 +0000

    Update system.h

--- system.h
+++ system.h
@@ -1,11 +1,16 @@
 #ifndef SYSTEM_H
 #define SYSTEM_H
 
+#include <string>
+#include <iostream>
 #include <vector>
 #include <random>
+#include <cmath>
 #include "disk.h"
 
 class System {
+//although not necessary, it is good practice to
+//make member variables private.
 private:
     double boxSize;
     double displacement;
@@ -13,13 +18,18 @@ private:
     std::mt19937 gen;
     std::uniform_real_distribution<double> dist;
 
+//member variables are accessed through public
+//member fucntions, thereby implementing data security.
 public:
-    System(int N, double displacement, double radius, double boxSize, int seed);
+    System(int N, double displacement,double radius, double boxSize, int seed);
     void step();
-    bool overlap(int i);
-    void enforceBoundaries(Disk &disk);
+
     void save(const std::string &filename);
-    double uniform(double a, double b);
+    
+    bool overlap(int i);
+    
+    void enforceBoundaries(Disk &disk);
+    double uniform(double min, double max);
 };
 
-#endif // SYSTEM_H
+#endif // !SYSTEM_H
